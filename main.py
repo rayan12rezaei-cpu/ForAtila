04d46b8c526e96c3e0393c30104b783e516695dc
RayanPythonMC <rayan12rezaei@gmail.com>
2026-07-25 22:07:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,76 @@
 import json
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit
+from PyQt5.QtCore import Qt
+
+try: json_path = f"{sys._MEIPASS}\\data.json"
+except: json_path = f"{os.path.abspath(".")}\\data.json"
+
+style = """
+
+QWidget#Win {
+    background-color: #212121;
+    border-radius: 10px;
+}
+QPushButton {
+    background-color: #f0f0f0;
+    color: #212121;
+    border-radius: 10px;
+}
+QPushButton#close {
+    background-color: red;
+    color: #212121;
+    border-radius: 10px;
+}
+QLineEdit {
+    background-color: #f0f0f0;
+    color: #212121;
+    border-radius: 10px;
+}
+QLabel {
+    color: #212121;
+}
+
+"""
+
+class ProvinceDetectorApp(QWidget):
+    def __init__(self):
+
+        global json_path
+        
+        super().__init__()
+
+        detector = PrvinceDetector(json_path)
+
+        self.setObjectName("Win")
+        
+        self.setWindowTitle("كدملي ياب براي آتيلا احمدزاده")
+
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        
+        self.main_layout = QVBoxLayout()
+
+        self.codeEntry = QLineEdit()
+        self.main_layout.addWidget(self.codeEntry)
+        
+        self.detectBtn = QPushButton("بررسي")
+        self.detectBtn.clicked.connect(self.detect)
+        self.main_layout.addWidget(self.detectBtn)
+
+        self.result = QLabel("استان")
+        self.main_layout.addWidget(self.result)
+
+        self.closeBtn = QPushButton("بستن")
+        self.closeBtn.clicked.connect(self.close)
+        self.closeBtn.setObjectName("close")
+        self.main_layout.addWidget(self.closeBtn)
+        
+        self.setLayout(self.main_layout)
+
+    def detect(self):
+        self.result.setText(self.detector.detect(self.codeEntry.text()))
+        self.update()
 
 class ProvinceDetector:
     def __init__(self, json_file: str):
@@ -32,10 +104,15 @@ class ProvinceDetector:
         for item in self.provinces_data:
             for start, end in item['ranges']:
                 if start <= prefix <= end:
+
                     return item['name']
         
         return "unknow | نامشخص"
 
-detector = ProvinceDetector('provinces.json')
 
-print(detector.detect(input("Enter National Code | کدملی را وارد کنید:")))
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyleSheet(style)
+    win = ProvinceDetectorApp()
+    win.show()
+    sys.exit(app.exec_())
